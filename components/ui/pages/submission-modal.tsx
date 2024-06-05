d5f8f68133964d31714fa962792717348231c2c7
zaid42005 <zaid.42005@gmail.com>
2024-06-05 19:04:03 -0400

    still aint workin

--- components/ui/pages/submission-modal.tsx
+++ components/ui/pages/submission-modal.tsx
@@ -14,7 +14,7 @@ import {
 
 const SubmissionModal = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen overflow-auto">
+    <div className="flex justify-center items-center min-h-screen overflow-auto w-full max-w-md mx-4 md:mr-8 my-6 p-6 bg-white rounded-lg shadow-lg">
       <Dialog>
         <DialogTrigger asChild>
           <Button>Send Message</Button>
