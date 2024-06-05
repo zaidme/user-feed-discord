a308cff8a93031e5d333ef34309db2e29443e59a
zaid42005 <zaid.42005@gmail.com>
2024-06-05 14:18:19 -0400

    enf workin

--- app/_actions/discord.ts
+++ app/_actions/discord.ts
@@ -7,10 +7,16 @@ export const sendDiscordMessage = async (data: {name: string, email:string, edit
     const { name, email, editor } = data;
  
     const message = `Name: ${name}\nEmail: ${email}\nMessage: ${editor}`;
+
+    const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+    
+    if (!webhookUrl) {
+      throw new Error('DISCORD_WEBHOOK_URL is not defined');
+    }
     // A fetch request to send data through the discord
     // webhook, and display it as a message in your
     // discord channel
-    await fetch("https://discordapp.com/api/webhooks/1247956688112324749/nLkyGZSIfCVS_uaNKiKyFOONCK_bzoWgu5Pqdp20T2pqt6vwqBjSqJFeFBI5kEVm3zlK", {
+    await fetch(webhookUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
